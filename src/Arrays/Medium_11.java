20b51e6dee81be2cf26bd007d94ab78b08668273
Prashant Bhandari <bhandari.prashant108@gmail.com>
2024-12-10 21:56:45 +0545

    Leetcode-Array-Medium11
    Link:https://leetcode.com/problems/container-with-most-water/?envType=problem-list-v2&envId=array&difficulty=EASY%2CMEDIUM

---
+++ src/Arrays/Medium_11.java
@@ -0,0 +1,36 @@
+package src.Arrays;
+
+import static java.lang.Math.min;
+
+class Medium_11 {
+    public int maxArea(int[] height) {
+
+        int max_area = 0;
+        int right = height.length - 1;
+        int left = 0;
+
+        while (left < right) {
+            int e_left = height[left];
+            int e_right = height[right];
+            int breadth = right - left;
+
+            int length = min(e_left, e_right);
+            int area = breadth * length;
+            if (area > max_area) max_area = area;
+
+            if (e_right > e_left) {
+                left++;
+            } else if (e_left > e_right) {
+                right--;
+            } else right--;
+        }
+        return (max_area);
+
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        Medium_11 obj = new Medium_11();
+        System.out.println(obj.maxArea(height));
+    }
+}
