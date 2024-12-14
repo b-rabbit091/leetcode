3b5afa4c3bcf7b315ebf8a013dded174a1d2cdac
b-rabbit091 <anilpanday908@gmail.com>
2024-12-14 23:25:23 +0545

    Leetcode-Array-Easy26
    Link:https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/1478759912/?envType=problem-list-v2&envId=array

---
+++ src/Arrays/Easy_26.java
@@ -0,0 +1,34 @@
+package src.Arrays;
+
+import java.util.Arrays;
+
+/*
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 2,
+with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+public class Easy_26 {
+    public int removeDuplicates(int[] nums) {
+
+        int index = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if(max<nums[i]){
+                max = nums[i];
+                nums[index]=nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        //int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int[] nums = {-3, -1, 0, 0};
+        Easy_26 obj = new Easy_26();
+        System.out.println(obj.removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
+    }
+}
