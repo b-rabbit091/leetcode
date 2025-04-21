9a03622c41e8b942fe740c19537d0b99ef3fdb65
b-rabbit091 <anilpanday908@gmail.com>
2025-04-21 16:26:56 -0500

    Leetcode-DP-Easy
    Link:https://leetcode.com/problems/pascals-triangle/description/?envType=problem-list-v2&envId=dynamic-programming

---
+++ src/DynamicProgramming/Easy/Easy_118.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+
+    def f(self, n, l):
+        if n == 2:
+            self.l[2] = [1, 1]
+            return [1, 1]
+
+        if self.l.get(n) is not None:
+            return l[n]
+
+        self.l[n - 1] = (self.f(n - 1, self.l))
+        a = []
+        for i in range(n):
+            elem = 1 if i == 0 or i == n - 1 else self.l[n - 1][i - 1] + self.l[n - 1][i]
+            a.append(elem)
+        self.l[n] = a
+        return a
+
+    def generate(self, numRows: int) -> List[List[int]]:
+        self.l = dict()
+        self.l[1] = [1]
+        self.f(numRows, self.l)
+        return list(self.l.values())
+
+
+s = Solution()
+print(s.generate(2))
